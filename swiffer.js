dff8e564f9279d6d7712dcb8429c3620055f6b54
Brandon Barber <brandonbarber@Brandons-MacBook-Pro.local>
2018-06-12 19:16:02 -0500

    init

---
+++ swiffer.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const { JSDOM } = require('jsdom');
+
+let file = fs.readFileSync(__dirname + '/' + process.argv[2]).toString('utf8');
+let jsdom = new JSDOM(file);
+
+const { window } = jsdom;
+
+[].slice
+  .call(window.document.querySelectorAll('style, link[rel=stylesheet]'))
+  .forEach(stylesheet => {
+    stylesheet.sheet.cssRules.forEach(rule => {
+      if (!window.document.querySelector(rule.selectorText)) {
+        console.log(rule.selectorText + ' is not in use.');
+      }
+    });
+  });
